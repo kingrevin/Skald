01a38fd9f3f99cc2e4e2d318b00bbf822fdee248
kingrevin <84926179+kingrevin@users.noreply.github.com>
2022-06-16 16:14:10 -0700

    added the last of the sounds and set them up for tuning

---
+++ Assets/Sounds/soundCue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundCue : MonoBehaviour
+{
+
+    public AudioSource Script;
+    public float tiemr = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tiemr >= 0)
+        {
+            tiemr -= Time.deltaTime;
+
+         
+        }
+        else if (tiemr <= 0)
+        {
+            
+        }
+    }
+}
